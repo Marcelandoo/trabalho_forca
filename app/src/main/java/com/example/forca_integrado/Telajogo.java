dd31bc79517ad7709ba85dc5db47699c2af51ddc
aluno <marcelohenricof@icloud.com>
2026-05-04 11:31:02 -0300

    Teclas e imagens

--- app/src/main/java/com/example/forca_integrado/Telajogo.java
+++ app/src/main/java/com/example/forca_integrado/Telajogo.java
@@ -1,6 +1,9 @@
 package com.example.forca_integrado;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Telajogo extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Telajogo extends AppCompatActivity implements View.OnClickListener{
+    private ImageView imagem;
+    private ArrayList<Integer> listaImagens;
+    private int indiceListaImagem;
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,33 @@ public class Telajogo extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        imagem = findViewById(R.id.imageView2);
+        indiceListaImagem = -1;
+        listaImagens = new ArrayList<Integer>();
+        listaImagens.add(R.drawable.forca_1_9);
+        listaImagens.add(R.drawable.forca_2_9);
+        listaImagens.add(R.drawable.forca_3_9);
+        listaImagens.add(R.drawable.forca_4_9);
+        listaImagens.add(R.drawable.forca_5_9);
+        listaImagens.add(R.drawable.forca_6_9);
+        listaImagens.add(R.drawable.forca_7_9);
+        listaImagens.add(R.drawable.forca_9_9);
+        listaImagens.add(R.drawable.forca_10_9);
+        listaImagens.add(R.drawable.forca_111_9);
+
+        b1 = findViewById(R.id.id1);
+        b1.setOnClickListener(this);
+    }
+
+    public void atualizarForca(){
+        indiceListaImagem++;
+        imagem.setImageResource(listaImagens.get(indiceListaImagem));
+    }
+
+    @Override
+    public void onClick(View v) {
+        atualizarForca();
+
     }
 }
