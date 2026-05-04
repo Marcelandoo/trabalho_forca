c3a5b1020824fb8fda18cbd6eacca59acaffc1df
aluno <marcelohenricof@icloud.com>
2026-05-04 11:31:02 -0300

    Teclas e imagens

--- app/src/main/java/com/example/forca_integrado/Telajogo.java
+++ app/src/main/java/com/example/forca_integrado/Telajogo.java
@@ -1,6 +1,10 @@
 package com.example.forca_integrado;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Telajogo extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Telajogo extends AppCompatActivity implements View.OnClickListener{
+    private ImageView imagem;
+    private ArrayList<Integer> listaImagens, listaIdsButtons;
+    private ArrayList<String> listaPalavras;
+    private int indiceListaImagem;
+    private TextView texto;
+    private Button b1;
+    private String palavra;
+    private char[] estado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,104 @@ public class Telajogo extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        imagem = findViewById(R.id.imageView2);
+        indiceListaImagem = -1;
+        listaImagens = new ArrayList<Integer>();
+        listaImagens.add(R.drawable.forca_1_9);
+        listaImagens.add(R.drawable.forca_2_9);
+        listaImagens.add(R.drawable.forca_3_9);
+        listaImagens.add(R.drawable.forca_4_9);
+        listaImagens.add(R.drawable.forca_5_9);
+        listaImagens.add(R.drawable.forca_6_9);
+        listaImagens.add(R.drawable.forca_7_9);
+        listaImagens.add(R.drawable.forca_9_9);
+        listaImagens.add(R.drawable.forca_10_9);
+        listaImagens.add(R.drawable.forca_111_9);
+
+        b1 = findViewById(R.id.id1);
+        b1.setOnClickListener(this);
+
+        listaPalavras = new ArrayList<String>();
+        listaPalavras.add("SOTAQUE");
+        listaPalavras.add("MATERIALISTA");
+        listaPalavras.add("SACRAMENTO");
+        listaPalavras.add("VELUDO");
+        listaPalavras.add("ANATOMIA");
+        listaPalavras.add("CAPACIDADE");
+        listaPalavras.add("ANOITECER");
+        listaPalavras.add("QUIMERA");
+        listaPalavras.add("ANDROIDE");
+        listaPalavras.add("TEXAS");
+
+        texto = findViewById(R.id.textView3);
+
+        listaIdsButtons = new ArrayList<Integer>();
+        listaIdsButtons.add(R.id.id1);
+        listaIdsButtons.add(R.id.id2);
+        listaIdsButtons.add(R.id.id3);
+        listaIdsButtons.add(R.id.id4);
+        listaIdsButtons.add(R.id.id5);
+        listaIdsButtons.add(R.id.id6);
+        listaIdsButtons.add(R.id.id7);
+        listaIdsButtons.add(R.id.id8);
+        listaIdsButtons.add(R.id.id9);
+        listaIdsButtons.add(R.id.id10);
+        listaIdsButtons.add(R.id.id11);
+        listaIdsButtons.add(R.id.id12);
+        listaIdsButtons.add(R.id.id13);
+        listaIdsButtons.add(R.id.id14);
+        listaIdsButtons.add(R.id.id15);
+        listaIdsButtons.add(R.id.id16);
+        listaIdsButtons.add(R.id.id17);
+        listaIdsButtons.add(R.id.id18);
+        listaIdsButtons.add(R.id.id19);
+        listaIdsButtons.add(R.id.id20);
+        listaIdsButtons.add(R.id.id21);
+        listaIdsButtons.add(R.id.id22);
+        listaIdsButtons.add(R.id.id23);
+        listaIdsButtons.add(R.id.id24);
+        listaIdsButtons.add(R.id.id25);
+        listaIdsButtons.add(R.id.id26);
+
+        for(int j = 0; j < listaIdsButtons.size(); j++){
+            Button b = findViewById(listaIdsButtons.get(j));
+            b.setOnClickListener(this);
+        }
+    }
+
+    public void inicializaJogo(){
+        imagem.setImageResource(R.drawable.forca_0_9);
+        palavra = sorteiaPalavra();
+        estado = new char[palavra.length()];
+        for(int i = 0; i < estado.length;i++){
+            estado[i] = '_';
+        }
+    }
+
+    public void atualizaTexto(){
+        String temporaria = new String();
+        temporaria = "";
+        for(int i = 0; i < estado.length; i++){
+            temporaria+= estado[i] + " ";
+        }
+        texto.setText(temporaria);
+    }
+
+    public String sorteiaPalavra(){
+        String retorno = new String();
+        Collections.shuffle(listaPalavras);
+        retorno = listaPalavras.get(0);
+        return retorno;
+    }
+
+    public void atualizarForca(){
+        indiceListaImagem++;
+        imagem.setImageResource(listaImagens.get(indiceListaImagem));
+    }
+
+    @Override
+    public void onClick(View v) {
+        atualizarForca();
     }
 }
